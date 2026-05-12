e7f21844b82ec0a2dd3f7fbd70fd13b86572d27d
Cássio <cassiojr0108@gmail.com>
2026-05-12 16:08:36 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import Fastify from 'fastify';
+
+const app = Fastify({
+    logger: true,
+});
+
+app.get('/', async () => {
+    return {
+        hello: 'world',
+    };
+});
+
+const start = async () => {
+    try {
+        await app.listen({
+            port: 3000,
+        });
+
+        console.log('Servidor rodando');
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
